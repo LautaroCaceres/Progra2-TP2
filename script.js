deb115ad7c2f9e075ab28c869048fa1901a419f9
LautaroCaceres <Lauti_ale_c@hotmail.com>
2026-06-18 16:50:09 -0300

    hero

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ document.addEventListener('click', (e) => {
     sportsMenu.classList.remove('sports-menu--open');
   }
 });
+
+// Hero carrusel
+const heroTrack = document.getElementById('heroTrack');
+const heroDots = document.getElementById('heroDots');
+const slides = heroTrack.querySelectorAll('.hero__slide');
+const dots = heroDots.querySelectorAll('.hero__dot');
+let currentSlide = 0;
+let heroInterval;
+
+function goToSlide(index) {
+  heroTrack.style.transform = `translateX(-${index * 100}%)`;
+  dots.forEach(d => d.classList.remove('hero__dot--active'));
+  dots[index].classList.add('hero__dot--active');
+  currentSlide = index;
+}
+
+function nextSlide() {
+  const next = (currentSlide + 1) % slides.length;
+  goToSlide(next);
+}
+
+function startAuto() {
+  heroInterval = setInterval(nextSlide, 5000);
+}
+
+function stopAuto() {
+  clearInterval(heroInterval);
+}
+
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    stopAuto();
+    goToSlide(i);
+    startAuto();
+  });
+});
+
+startAuto();
